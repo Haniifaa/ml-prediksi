18d0c56d77b9f2637fbc3822a04a0ec3ddb56069
Haniifa Aliila Faudyta <133667191+Haniifaa@users.noreply.github.com>
2024-05-21 18:04:53 +0700

    Update ClientError.js

--- src/exceptions/ClientError.js
+++ src/exceptions/ClientError.js
@@ -9,12 +9,3 @@ class ClientError extends Error {
 module.exports = ClientError;
 
 
-// class ClientError extends Error {
-//     constructor(message, statusCode = 400) {
-//         super(message);
-//         this.statusCode = statusCode;
-//         this.name = 'ClientError';
-//     }
-// }
- 
-// module.exports = ClientError;
