d3e0ec32468b12963d0e1b3bd4b43116ab3e3dab
Haniifa Aliila Faudyta <133667191+Haniifaa@users.noreply.github.com>
2024-05-21 18:06:28 +0700

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -26,37 +26,3 @@ const routes = [
 module.exports = routes;
 
 
-
-
-// const postPredictHandler = require('../server/handler');
- 
-// const routes = [
-//   {
-//     path: '/predict',
-//     method: 'POST',
-//     handler: postPredictHandler,
-//     options: {
-//       payload: {
-        
-//         maxBytes: 1000000, // 1MB
-//         output: 'stream',
-//                 parse: true,
-//                 allow: 'multipart/form-data',
-//                 multipart: true,
-              
-        
-        // failAction: (request, h, err) => {
-        //     if (err.output.statusCode === 413) {
-        //         return h.response({
-        //             status: 'fail',
-        //             message: 'Payload content length greater than maximum allowed: 1000000'
-        //         }).code(413).takeover();
-        //     }
-        //     throw err;
-        // }
-//       }
-//     }
-//   }
-// ]
- 
-// module.exports = routes;
