ad25769e0deda86313d53d0f6eee800f8158b444
Haniifa Aliila Faudyta <133667191+Haniifaa@users.noreply.github.com>
2024-05-21 18:07:58 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -20,24 +20,17 @@ async function predictClassification(model, image) {
         const label = cancerProbability > 0.5 ? 'Cancer' : 'Non-cancer';
         //const classResult = tf.argMax(prediction, 1).dataSync()[0];
         //const label = classes[classResult];
-//   if (confidenceScore > 90){
-//     label= 'Cancer';
-//   }
-//   else{
-//     label= 'Non-cancer';
-//   }
-
   
 
         let suggestion;
 
         if (label === 'Cancer') {
-            // && confidenceScore > 99
+            
             suggestion = "Segera periksa ke dokter!"
         } 
-        // if (label === 'Non-Cancer' && confidenceScore <= 99){
-        if (label === 'Non-Cancer') {
-            // && confidenceScore <= 90
+        
+        if (label === 'Non-cancer') {
+           
             suggestion = "Tidak perlu khawatir, tetap jaga kesehatan!"
         }
         return { label, suggestion };
@@ -52,37 +45,3 @@ async function predictClassification(model, image) {
 module.exports = predictClassification;
 
 
-
-// const tf = require('@tensorflow/tfjs-node');
-// const InputError = require('../exceptions/InputError');
-
-// async function predictClassification(model, image) {
-//   try {
-//     const tensor = tf.node
-//     .decodeJpeg(image)
-//     .resizeNearestNeighbor([224, 224])
-//     .expandDims()
-//     .toFloat()
-
-//     const prediction = model.predict(tensor);
-//     const score = await prediction.data();
-//     const label = score > 0.5 ? 'Cancer' : 'Non-cancer';
-
-//     let suggestion;
- 
-//   if (label === 'Cancer') {
-    
-//     suggestion = "Segera konsultasi dengan dokter terdekat"
-//   }
- 
-//   if (label === 'Non-cancer') {
-    
-//     suggestion = "Tidak ada cancer yang terdeteksi, tetap jaga kesehatan"
-//   }
-//   return { label, suggestion };
-//   } catch(error){
-//     throw new InputError(`${error.message}`)
-//   }
-// }
-
-// module.exports = predictClassification;
