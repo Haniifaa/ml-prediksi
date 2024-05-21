c6956f0673a6947f9a1dba892fce61b38ad8b4fe
Haniifa Aliila Faudyta <133667191+Haniifaa@users.noreply.github.com>
2024-05-21 18:05:44 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -8,7 +8,7 @@ async function postPredictHandler(request, h) {
     const { model } = request.server.app;
 
     const { label, suggestion } = await predictClassification(model, image);
-    // , confidenceScore
+    
     // confidenceScore
     const id = crypto.randomUUID();
     const createdAt = new Date().toISOString();
@@ -36,51 +36,3 @@ module.exports = postPredictHandler;
 
 
 
-// const predictClassification = require('../services/inferenceService');
-// const crypto = require('crypto');
-// const storeData = require('../services/storeData');
-
-// async function postPredictHandler(request, h) {
-//     const { image } = request.payload;
-//     const { model } = request.server.app;
-
-//     if (image.hapi.headers['content-length'] > 1000000) {
-//         return h.response({
-//             status: 'fail',
-//             message: 'Payload content length greater than maximum allowed: 1000000'
-//         }).code(413);
-//     }
-
-//     try{
-//     const { label, suggestion } = await predictClassification(model, image);
-
-//     const id = crypto.randomUUID();
-//   const createdAt = new Date().toISOString();
- 
-//   const data = {
-//     "id": id,
-//     "result": label,
-//     "suggestion": suggestion,
-//     "createdAt": createdAt
-//   }
-
-//   await storeData (id, data);
-
-
-//   const response = h.response({
-//     status: 'success',
-//     message: 'Model is predicted successfully',
-//     data
-//   })
-//   response.code(201);
-
-//   return response;
-// } catch (error) {
-//     return h.response({
-//         status: 'fail',
-//         message: 'Terjadi kesalahan dalam melakukan prediksi'
-//     }).code(400);
-// }
-
-// };
-// module.exports = postPredictHandler;
